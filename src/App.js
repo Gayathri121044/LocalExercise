1341ffc37ffaea0254b71cb7ae07b3507895939a
Gayathri121044 <115973005+Gayathri121044@users.noreply.github.com>
2022-10-20 15:58:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import GetInput from "./UserInputs";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Resume builder</h1>
+      <GetInput />
+    </div>
+  );
+}
---
+++ src/UserInputs.js
@@ -0,0 +1,18 @@
+import React from "react";
+export default class GetInput extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      adress: "",
+      skills: "",
+      experience: "",
+      education: ""
+    };
+  }
+  render() {
+    return <div>heli</div>;
+  }
+}
